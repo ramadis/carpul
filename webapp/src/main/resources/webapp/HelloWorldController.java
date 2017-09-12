6d93368f8bd3945a0f084d8184e8ac96c6a9a970
ramadis <rramiro.o@hotmail.com>
2017-09-12 09:16:32 -0300

    And more configurations

---
+++ webapp/src/main/resources/webapp/HelloWorldController.java
@@ -0,0 +1,11 @@
+package webapp;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/")
+	public ModelAndView helloWorld() {
+		final ModelAndView mav = new ModelAndView("index");
+		mav.addObject("greeting", "PAW");
+		return mav;
+	}
+}
---
+++ webapp/src/main/resources/webapp/WebConfig.java
@@ -0,0 +1,15 @@
+package webapp;
+
+@EnableWebMvc
+@ComponentScan({ "webapp.controller" })
+@Configuration
+public class WebConfig {
+	@Bean
+	public ViewResolver viewResolver() {
+		final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
