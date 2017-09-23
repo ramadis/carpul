de5c9da058a2bf67d17d1cf77d16732db44e2827
ramadis <rramiro.o@hotmail.com>
2017-09-23 16:28:56 -0300

    Loading CSS

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -19,8 +21,14 @@ import javax.sql.DataSource;
 })
 @EnableWebMvc
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
+	
+	 @Override
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	  registry.addResourceHandler("/static/**").addResourceLocations("/static/").setCachePeriod(31556926);
+	 }
+	 
 	@Bean
 	public ViewResolver viewResolver() {
 		final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
