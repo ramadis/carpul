0faf7532619f34df94649e7bf1954404d7b9a12b
ramadis <rramiro.o@hotmail.com>
2019-12-05 18:03:34 -0300

    Add suggestions endpoint

--- spa/parcel-react-boilerplate/src/services/Search.js
+++ spa/parcel-react-boilerplate/src/services/Search.js
@@ -1,16 +1,29 @@
-import { GETwithAuth } from './Utils'
+import { GETwithAuth } from "./Utils";
 
 export const search = async ({ to, from, when }) => {
-  const query = encodeURIComponent({ to, from, when })
+  const query = encodeURIComponent({ to, from, when });
 
   const results = await GETwithAuth(
     `/search?to=${to}&from=${from}&when=${when}`
   ).then(res => {
     if (res.isRawResponse) {
       // TODO: Handle specific error messages
-      return
+      return;
     }
-    return res
-  })
-  return results
-}
+    return res;
+  });
+  return results;
+};
+
+export const getSuggestions = async ({ origin }) => {
+  const results = await GETwithAuth(
+    `/search/suggestions?origin=${origin}`
+  ).then(res => {
+    if (res.isRawResponse) {
+      // TODO: Handle specific error messages
+      return;
+    }
+    return res;
+  });
+  return results;
+};
