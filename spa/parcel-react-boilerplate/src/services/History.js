e715ae2c1403f2125f75b05aa1759151107a801b
ramadis <rramiro.o@hotmail.com>
2019-12-10 23:36:28 -0300

    Add errors to get history

--- spa/parcel-react-boilerplate/src/services/History.js
+++ spa/parcel-react-boilerplate/src/services/History.js
@@ -3,8 +3,21 @@ import { GETwithAuth } from "./Utils";
 export const getHistoryByUser = async id => {
   const history = await GETwithAuth(`/users/${id}/history`).then(res => {
     if (res.isRawResponse) {
-      // TODO: Handle specific error messages
-      return;
+      const errors = {
+        404: {
+          title: "We can't find the user",
+          subtitle: "You sure you are trying to access the correct one?",
+        },
+        default: {
+          title: "Something went wrong",
+          subtitle: "And we don't know what it is, sorry :(.",
+        },
+      };
+
+      throw {
+        message: errors[res.status] || errors.default,
+        code: res.status,
+      };
     }
     return res;
   });
