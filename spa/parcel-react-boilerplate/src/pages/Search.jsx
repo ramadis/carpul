39abff943b0e497ccf280263de1b46a64e54c085
ramadis <rramiro.o@hotmail.com>
2019-12-12 12:55:22 -0300

    Fix query params issue

--- spa/parcel-react-boilerplate/src/pages/Search.jsx
+++ spa/parcel-react-boilerplate/src/pages/Search.jsx
@@ -201,7 +201,11 @@ const encodeQueryParams = function(obj) {
   var str = [];
   for (var p in obj) {
     if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      str.push(
+        encodeURIComponent(p) +
+          "=" +
+          encodeURIComponent(obj[p] == null ? "" : obj[p])
+      );
     }
   }
   return str.join("&");
