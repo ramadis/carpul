594a042c33992bf77ac494afbbd8d8ad932d6e6f
ramadis <rramiro.o@hotmail.com>
2019-12-15 16:56:07 -0300

    Home sending latitude and longitude if any

--- webapp/frontend/src/services/routes.js
+++ webapp/frontend/src/services/routes.js
@@ -1,4 +1,13 @@
 export const routes = {
-  search: ({ from, to, date = new Date() }) =>
-    `/search?from=${from.city}&to=${to.city}&when=${date && date.getTime()}`,
+  search: ({ from, to, date = new Date() }) => {
+    console.log(from);
+    const depLat = (from.position && from.position.latitude) || "";
+    const depLon = (from.position && from.position.longitude) || "";
+    const arrLat = (to.position && to.position.latitude) || "";
+    const arrLon = (to.position && to.position.longitude) || "";
+    return `/search?from=${from.city}&to=${
+      to.city
+    }&depLat=${depLat}&depLon=${depLon}&arrLat=${arrLat}&arrLon=${arrLon}&when=${date &&
+      date.getTime()}`;
+  },
 };
